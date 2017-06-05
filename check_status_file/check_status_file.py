7ea4d955314a50aa3a787fc00c13e12d58388db5
--get <--get>
2017-06-05 08:44:36 +0200

    Fixe Python indentation

--- check_status_file/check_status_file.py
+++ check_status_file/check_status_file.py
@@ -15,125 +15,124 @@ STATUS_CRITICAL =  2
 
 # String representations of valid status codes
 STATUS_CODES = {
-  "OK":       STATUS_OK,
-  "WARNING":  STATUS_WARNING,
-  "ERROR":    STATUS_CRITICAL,
-  "CRITICAL": STATUS_CRITICAL
+    "OK":       STATUS_OK,
+    "WARNING":  STATUS_WARNING,
+    "ERROR":    STATUS_CRITICAL,
+    "CRITICAL": STATUS_CRITICAL
 }
 
 def print_stdout(string_to_print):
-  print(string_to_print)
+    print(string_to_print)
 
 def get_timedelta_from_now(other_timestamp):
-  print("\n==>", datetime.datetime.now(), datetime.datetime.utcnow(), other_timestamp)
-  # If other timestamp is naive, we assume that it is in the same timezone as
-  # local system. If it contains timezone information, we convert local time to UTC
-  # with tzinfo to calculate the difference
-  # 
-  # http://techblog.thescore.com/2015/11/03/timezones-in-python/
-  # http://pytz.sourceforge.net/#localized-times-and-date-arithmetic
-  # https://stackoverflow.com/questions/5802108/how-to-check-if-a-datetime-object-is-localized-with-pytz
-  if (other_timestamp.tzinfo is None) or (other_timestamp.tzinfo.utcoffset(other_timestamp) is None):
-    print("naive")
-    # Naive
-    return((datetime.datetime.now() - other_timestamp).total_seconds())
-  else:
-    print("timezone")
-    # With timezone
-    return((pytz.utc.localize(datetime.datetime.utcnow()) - other_timestamp).total_seconds())
-
+    print("\n==>", datetime.datetime.now(), datetime.datetime.utcnow(), other_timestamp)
+    # If other timestamp is naive, we assume that it is in the same timezone as
+    # local system. If it contains timezone information, we convert local time to UTC
+    # with tzinfo to calculate the difference
+    #
+    # http://techblog.thescore.com/2015/11/03/timezones-in-python/
+    # http://pytz.sourceforge.net/#localized-times-and-date-arithmetic
+    # https://stackoverflow.com/questions/5802108/how-to-check-if-a-datetime-object-is-localized-with-pytz
+    if (other_timestamp.tzinfo is None) or (other_timestamp.tzinfo.utcoffset(other_timestamp) is None):
+        print("naive")
+        # Naive
+        return((datetime.datetime.now() - other_timestamp).total_seconds())
+    else:
+        print("timezone")
+        # With timezone
+        return((pytz.utc.localize(datetime.datetime.utcnow()) - other_timestamp).total_seconds())
 
 
 def check_file(status_file_name, warning_hours, critical_hours):
-  if not(os.path.isfile(status_file_name)):
-    print_stdout("CRITICAL: status file '{}' does not exist".format(status_file_name))
-    return(STATUS_CRITICAL)
+    if not(os.path.isfile(status_file_name)):
+        print_stdout("CRITICAL: status file '{}' does not exist".format(status_file_name))
+        return(STATUS_CRITICAL)
 
-  # Status data is expected as a single semicolon-delimited line in the
-  # following format:
-  # <timestamp>;<status>;<text description>
-  # Timestamp is in ISO 8601 format.
-  # Status can be one of the following values: OK, WARNING, ERROR, CRITICAL
-  with open(status_file_name, "r") as f:
-    status_data = f.readline().split(";")
+    # Status data is expected as a single semicolon-delimited line in the
+    # following format:
+    # <timestamp>;<status>;<text description>
+    # Timestamp is in ISO 8601 format.
+    # Status can be one of the following values: OK, WARNING, ERROR, CRITICAL
+    with open(status_file_name, "r") as f:
+        status_data = f.readline().split(";")
 
-  if len(status_data) != 3:
-    print_stdout("CRITICAL: wrong status data in '{}'".format(status_file_name))
-    return(STATUS_CRITICAL)
+    if len(status_data) != 3:
+        print_stdout("CRITICAL: wrong status data in '{}'".format(status_file_name))
+        return(STATUS_CRITICAL)
 
-  try:
-    status_timestamp = dateutil.parser.parse(status_data[0])
-  except ValueError as e:
-    print_stdout("Wrong date/time format in file '{}': {}".format(status_file_name, status_data[0]))
-    return(STATUS_CRITICAL)
+    try:
+        status_timestamp = dateutil.parser.parse(status_data[0])
+    except ValueError as e:
+        print_stdout("Wrong date/time format in file '{}': {}".format(status_file_name, status_data[0]))
+        return(STATUS_CRITICAL)
 
-  try:
-    status_code = STATUS_CODES[status_data[1].upper()]
-  except KeyError as e:
-    print_stdout("Wrong status code in file '{}': {}".format(status_file_name, status_data[1]))
-    return(STATUS_CRITICAL)
+    try:
+        status_code = STATUS_CODES[status_data[1].upper()]
+    except KeyError as e:
+        print_stdout("Wrong status code in file '{}': {}".format(status_file_name, status_data[1]))
+        return(STATUS_CRITICAL)
 
-  status_age = get_timedelta_from_now(status_timestamp)
-  status_age_hours_str = "{:.1f} hour(s)".format(status_age / 3600)
+    status_age = get_timedelta_from_now(status_timestamp)
+    status_age_hours_str = "{:.1f} hour(s)".format(status_age / 3600)
 
-  print("===>", datetime.datetime.utcnow())
-  dummy = pytz.utc.localize(datetime.datetime.utcnow()) - status_timestamp
-  print("===>", dummy)
-  print("===>", dummy.total_seconds())
-  print("===>", dummy.seconds)
-  return_value = STATUS_UNKNOWN
+    print("===>", datetime.datetime.utcnow())
+    dummy = pytz.utc.localize(datetime.datetime.utcnow()) - status_timestamp
+    print("===>", dummy)
+    print("===>", dummy.total_seconds())
+    print("===>", dummy.seconds)
+    return_value = STATUS_UNKNOWN
 
-  if status_age <= datetime.timedelta(hours=warning_hours).seconds:
-    # Last status change is under the warning threshold
-    if status_code == STATUS_OK:
-      print_stdout("OK - {} ({} ago)".format(status_data[2], status_age_hours_str))
-      return_value = STATUS_OK
+    if status_age <= datetime.timedelta(hours=warning_hours).seconds:
+        # Last status change is under the warning threshold
+        if status_code == STATUS_OK:
+            print_stdout("OK - {} ({} ago)".format(status_data[2], status_age_hours_str))
+            return_value = STATUS_OK
+        else:
+            pass
+    elif status_age <= datetime.timedelta(hours=critical_hours).seconds:
+        pass
     else:
-      pass
-  elif status_age <= datetime.timedelta(hours=critical_hours).seconds:
-    pass
-  else:
-    pass
+        pass
 
-  print(status_age, status_age_hours_str)
-  print(status_code)
-  print(status_timestamp)
-  print(str(status_timestamp))
-  print("We are the {:%d, %b %Y}".format(status_timestamp))
-  return(return_value)
-  # http://techblog.thescore.com/2015/11/03/timezones-in-python/
-  # http://pytz.sourceforge.net/#localized-times-and-date-arithmetic
+    print(status_age, status_age_hours_str)
+    print(status_code)
+    print(status_timestamp)
+    print(str(status_timestamp))
+    print("We are the {:%d, %b %Y}".format(status_timestamp))
+    return(return_value)
+    # http://techblog.thescore.com/2015/11/03/timezones-in-python/
+    # http://pytz.sourceforge.net/#localized-times-and-date-arithmetic
 
-  # import datetime
-  # import dateutil.parser
-  # import pytz
+    # import datetime
+    # import dateutil.parser
+    # import pytz
 
-  # check_date = dateutil.parser.parse("2017-05-30T11:12:05+02:00")
-  # local_date = pytz.utc.localize(datetime.datetime.utcnow())
-  # (local_date - check_date).total_seconds()
+    # check_date = dateutil.parser.parse("2017-05-30T11:12:05+02:00")
+    # local_date = pytz.utc.localize(datetime.datetime.utcnow())
+    # (local_date - check_date).total_seconds()
 
 def main():
-  exit_code = STATUS_UNKNOWN
-  try:
-    parser = argparse.ArgumentParser(description="Nagios plugin to report status from custom file")
-    parser.add_argument("status_file_name", help="File to read status information from")
-    parser.add_argument('-w', '--warning-hours', dest='warning_hours', type=int, default=25,
-      help='Number of hours since last change to cause warning state (default: %(default)d)')
-    parser.add_argument('-c', '--critical-hours', dest='critical_hours', type=int, default=49,
-      help='Number of hours since last change to cause critical state (default: %(default)d)')
+    exit_code = STATUS_UNKNOWN
+    try:
+        parser = argparse.ArgumentParser(description="Nagios plugin to report status from custom file")
+        parser.add_argument("status_file_name", help="File to read status information from")
+        parser.add_argument('-w', '--warning-hours', dest='warning_hours', type=int, default=25,
+          help='Number of hours since last change to cause warning state (default: %(default)d)')
+        parser.add_argument('-c', '--critical-hours', dest='critical_hours', type=int, default=49,
+          help='Number of hours since last change to cause critical state (default: %(default)d)')
 
-    options = parser.parse_args()
-    if options.warning_hours > options.critical_hours:
-      print("Error: critical threshold cannot be less than warning")
-      return (STATUS_UNKNOWN)
+        options = parser.parse_args()
+        if options.warning_hours > options.critical_hours:
+            print("Error: critical threshold cannot be less than warning")
+            return (STATUS_UNKNOWN)
 
-    exit_code = check_file(options.status_file_name, options.warning_hours,
-      options.critical_hours)
+        exit_code = check_file(options.status_file_name, options.warning_hours,
+          options.critical_hours)
 
-  except Exception as e:
-    print("Unhandled exception: {}".format(e))
+    except Exception as e:
+        print("Unhandled exception: {}".format(e))
 
-  return (exit_code)
+    return (exit_code)
 
 if __name__ == '__main__':
-  sys.exit(main())
+    sys.exit(main())
--- check_status_file/test_check_status_file.py
+++ check_status_file/test_check_status_file.py
@@ -8,9 +8,9 @@ import dateutil.tz
 import check_status_file
 
 if sys.version_info >= (3,0,0):
-  BUILTIN_OPEN_NAME = "builtins.open"
+    BUILTIN_OPEN_NAME = "builtins.open"
 else:
-  BUILTIN_OPEN_NAME = "__builtin__.open"
+    BUILTIN_OPEN_NAME = "__builtin__.open"
 
 class get_timedelta_from_now_UnitTests(unittest.TestCase):
     """Unit tests for 'get_timedelta_from_now' function"""
@@ -52,55 +52,55 @@ class get_timedelta_from_now_UnitTests(unittest.TestCase):
 @mock.patch("os.path.isfile")
 @mock.patch("check_status_file.print_stdout")
 class check_file_UnitTests(unittest.TestCase):
-  """Unit tests for 'check_file' function"""
+    """Unit tests for 'check_file' function"""
 
-  def test_file_doesnt_exist(self, print_stdout_mock, os_path_isfile_mock):
-    """Should return critical status if status file doesn't exist"""
-    os_path_isfile_mock.return_value = False
-    ret_val = check_status_file.check_file("file_name", 10, 20)
-    self.assertEqual(ret_val, 2)
-    print_stdout_mock.assert_called_with("CRITICAL: status file 'file_name' does not exist")
+    def test_file_doesnt_exist(self, print_stdout_mock, os_path_isfile_mock):
+        """Should return critical status if status file doesn't exist"""
+        os_path_isfile_mock.return_value = False
+        ret_val = check_status_file.check_file("file_name", 10, 20)
+        self.assertEqual(ret_val, 2)
+        print_stdout_mock.assert_called_with("CRITICAL: status file 'file_name' does not exist")
 
-  # Mocked readline fails on empty file (https://github.com/testing-cabal/mock/issues/382)
-  # Because of that we test empty file condition only in functional test
-  @mock.patch(BUILTIN_OPEN_NAME, new_callable=mock.mock_open, read_data="wrong_data")
-  @freezegun.freeze_time("2012-01-14 03:21:34", tz_offset=-4)
-  def test_file_exists_wrong_data(self, open_mock, print_stdout_mock, os_path_isfile_mock):
-    """Should return critical status if status file doesn't contain valid data"""
+    # Mocked readline fails on empty file (https://github.com/testing-cabal/mock/issues/382)
+    # Because of that we test empty file condition only in functional test
+    @mock.patch(BUILTIN_OPEN_NAME, new_callable=mock.mock_open, read_data="wrong_data")
+    @freezegun.freeze_time("2012-01-14 03:21:34", tz_offset=-4)
+    def test_file_exists_wrong_data(self, open_mock, print_stdout_mock, os_path_isfile_mock):
+        """Should return critical status if status file doesn't contain valid data"""
 
-    os_path_isfile_mock.return_value = True
+        os_path_isfile_mock.return_value = True
 
-    self.assertEqual(check_status_file.check_file("file_name", 10, 20), 2)
-    print_stdout_mock.assert_called_with("CRITICAL: wrong status data in 'file_name'")
+        self.assertEqual(check_status_file.check_file("file_name", 10, 20), 2)
+        print_stdout_mock.assert_called_with("CRITICAL: wrong status data in 'file_name'")
 
-    open_mock.side_effect = [mock.mock_open(read_data="wrong_date;status;description").return_value]
-    self.assertEqual(check_status_file.check_file("file_name", 10, 20), 2)
-    print_stdout_mock.assert_called_with("Wrong date/time format in file 'file_name': wrong_date")
+        open_mock.side_effect = [mock.mock_open(read_data="wrong_date;status;description").return_value]
+        self.assertEqual(check_status_file.check_file("file_name", 10, 20), 2)
+        print_stdout_mock.assert_called_with("Wrong date/time format in file 'file_name': wrong_date")
 
-    open_mock.side_effect = [mock.mock_open(read_data="2017-05-30T11:12:05+02:00;wrong_status;description").return_value]
-    self.assertEqual(check_status_file.check_file("file_name", 10, 20), 2)
-    print_stdout_mock.assert_called_with("Wrong status code in file 'file_name': wrong_status")
+        open_mock.side_effect = [mock.mock_open(read_data="2017-05-30T11:12:05+02:00;wrong_status;description").return_value]
+        self.assertEqual(check_status_file.check_file("file_name", 10, 20), 2)
+        print_stdout_mock.assert_called_with("Wrong status code in file 'file_name': wrong_status")
 
-    # freezegun.freeze_time("2014-01-14 12:00:01")
-    # Both upper and lowercase codes are fine
-    open_mock.side_effect = [mock.mock_open(read_data="2017-05-30T11:12:05+02:00;OK;description").return_value]
-    self.assertEqual(check_status_file.check_file("file_name", 10, 20), 0)
-
-
-    open_mock.side_effect = [mock.mock_open(read_data="2017-05-30T11:12:05+02:00;ok;description").return_value]
-    self.assertEqual(check_status_file.check_file("file_name", 10, 20), 0)
+        # freezegun.freeze_time("2014-01-14 12:00:01")
+        # Both upper and lowercase codes are fine
+        open_mock.side_effect = [mock.mock_open(read_data="2017-05-30T11:12:05+02:00;OK;description").return_value]
+        self.assertEqual(check_status_file.check_file("file_name", 10, 20), 0)
 
 
-
-    #TODO: remove
-    # open_mock.side_effect = [mock.mock_open(read_data="2017-05-30T11:12:05;ok;description").return_value]
-    # self.assertEqual(check_status_file.check_file("file_name", 10, 20), -1)
+        open_mock.side_effect = [mock.mock_open(read_data="2017-05-30T11:12:05+02:00;ok;description").return_value]
+        self.assertEqual(check_status_file.check_file("file_name", 10, 20), 0)
 
 
 
+        #TODO: remove
+        # open_mock.side_effect = [mock.mock_open(read_data="2017-05-30T11:12:05;ok;description").return_value]
+        # self.assertEqual(check_status_file.check_file("file_name", 10, 20), -1)
 
-    # open_mock.side_effect = (mock.mock_open().return_value,)
-    # check_status_file.check_file("file_name", 10, 20)
+
+
+
+        # open_mock.side_effect = (mock.mock_open().return_value,)
+        # check_status_file.check_file("file_name", 10, 20)
 
 
 # 2Check
