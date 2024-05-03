0e435fba3c759c812177dcbdbd7a7877bbc1e6f3
cheretbe <a.a.orlov@gmail.com>
2024-05-03 23:28:19 +0200

    check_balance: return check status directly instead of writing status file

--- check_balance/check_balance.py
+++ check_balance/check_balance.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python
+#!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
 import os
@@ -12,6 +12,12 @@ import argparse
 import locale
 import traceback
 import lxml.etree
+
+# Nagios status codes
+STATUS_UNKNOWN  = -1
+STATUS_OK       =  0
+STATUS_WARNING  =  1
+STATUS_CRITICAL =  2
 
 locale.setlocale(locale.LC_ALL, '')
 
@@ -27,14 +33,6 @@ def print_verbose(verbose_msg):
 def check_http_reply(reply):
     if reply.status_code != 200:
         raise Exception("HTTP call has failed. Status code: {code} - {text}".format(code=reply.status_code, text=reply.text))
-
-def write_status(status_code, status_text):
-    print_with_timestamp("Status {} - {}".format(status_code,status_text))
-
-    if not options is None:
-        with open(options.status_file_name, "w") as f:
-            timestamp = datetime.datetime.now(dateutil.tz.tzoffset(None, -time.altzone)).replace(microsecond=0).isoformat()
-            f.write(timestamp + ";" + status_code + ";" + status_text)
 
 def get_next_withdrawal_date():
     if datetime.date.today().day >= options.withdrawal_day:
@@ -116,26 +114,25 @@ def do_check_balance():
         )
         if balance < options.withdrawal_amount:
             if days_until_withdrawal < options.critical_threshold:
-                write_status("CRITICAL", status_text)
+                return (STATUS_CRITICAL, status_text)
             elif days_until_withdrawal < options.warning_threshold:
-                write_status("WARNING", status_text)
+                return (STATUS_WARNING, status_text)
             else:
-                write_status("OK", status_text)
+                return (STATUS_OK, status_text)
         else:
-            write_status("OK", status_text)
+            return (STATUS_OK, status_text)
     else:
         if balance < options.critical_threshold:
-            write_status("CRITICAL", "Balance {} is less than critical threshold of {}".format(balance, options.critical_threshold))
+            return (STATUS_CRITICAL, "Balance {} is less than critical threshold of {}".format(balance, options.critical_threshold))
         elif balance < options.warning_threshold:
-            write_status("WARNING", "Balance {} is less than warning threshold of {}".format(balance, options.warning_threshold))
+            return (STATUS_WARNING, "Balance {} is less than warning threshold of {}".format(balance, options.warning_threshold))
         else:
-            write_status("OK", "Balance is {}".format(balance))
+            return (STATUS_OK, "Balance is {}".format(balance))
 
 def main():
-    exit_code = 0
+    exit_code = STATUS_OK
     try:
         parser = argparse.ArgumentParser(description="Custom script to check balance and write status to a file")
-        parser.add_argument("status_file_name", help="File to write status information to")
         parser.add_argument("login", help="User name")
         parser.add_argument("password", help="password")
         parser.add_argument('-p', '--provider', dest='provider', default="dialog",
@@ -154,7 +151,8 @@ def main():
         global options
         options = parser.parse_args()
         if options.warning_threshold < options.critical_threshold:
-            write_status("UNKNOWN", "Error: warning threshold cannot be less than critical")
+            exit_code = STATUS_UNKNOWN
+            print("Error: warning threshold cannot be less than critical")
 
         if options.provider == "dialog-new":
             if options.warning_threshold == 200:
@@ -162,16 +160,17 @@ def main():
             if options.critical_threshold == 100:
                 options.critical_threshold = 5
 
-        print_with_timestamp("Checking balance for user {} on {}".format(options.login, options.provider))
-        print_verbose("Status file name: {}".format(options.status_file_name))
+        print_verbose("Checking balance for user {} on {}".format(options.login, options.provider))
 
-        do_check_balance()
+        check_status,check_message = do_check_balance()
+        exit_code = check_status
+        print(check_message)
 
     except Exception as e:
         if options.verbose:
             print(traceback.format_exc())
-        write_status("CRITICAL", "Unhandled exception: {}".format(e))
-        exit_code = 1
+        print("Unhandled exception: {}".format(e))
+        exit_code = STATUS_CRITICAL
 
     return (exit_code)
 
