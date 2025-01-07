25abc514af643d5f48a110e8ee3c483bed77cb76
cheretbe <a.a.orlov@gmail.com>
2025-01-07 15:48:30 +0200

    check_balance: Add last withdrawal amount check + black code formatting

---
+++ check_balance/README.md
@@ -0,0 +1,4 @@
+```shell
+read -s -p "Password: " my_pwd; echo ""; export my_pwd
+check_balance/check_balance.py 0000000 $my_pwd -p dialog-new -a 525 -d 13 -v
+```
--- check_balance/check_balance.py
+++ check_balance/check_balance.py
@@ -14,63 +14,83 @@ import traceback
 import lxml.etree
 
 # Nagios status codes
-STATUS_UNKNOWN  = -1
-STATUS_OK       =  0
-STATUS_WARNING  =  1
-STATUS_CRITICAL =  2
+STATUS_UNKNOWN = -1
+STATUS_OK = 0
+STATUS_WARNING = 1
+STATUS_CRITICAL = 2
 STATUS_NAMES = {
     STATUS_OK: "OK",
     STATUS_WARNING: "WARNING",
     STATUS_CRITICAL: "CRITICAL",
-    STATUS_UNKNOWN: "UNKNOWN"
+    STATUS_UNKNOWN: "UNKNOWN",
 }
 
-locale.setlocale(locale.LC_ALL, '')
+locale.setlocale(locale.LC_ALL, "")
 
 options = None
 
+
 def print_with_timestamp(msg):
-    print (u"{} {}".format(datetime.datetime.now().strftime("%x %X"), msg))
+    print("{} {}".format(datetime.datetime.now().strftime("%x %X"), msg))
+
 
 def print_verbose(verbose_msg):
     if options.verbose:
         print_with_timestamp(verbose_msg)
 
+
 def check_http_reply(reply):
     if reply.status_code != 200:
-        raise Exception("HTTP call has failed. Status code: {code} - {text}".format(code=reply.status_code, text=reply.text))
+        raise Exception(
+            "HTTP call has failed. Status code: {code} - {text}".format(
+                code=reply.status_code, text=reply.text
+            )
+        )
+
 
 def get_next_withdrawal_date():
     if datetime.date.today().day >= options.withdrawal_day:
         month_offset = 1
     else:
         month_offset = 0
-    return datetime.date.today() + dateutil.relativedelta.relativedelta(months=month_offset, day=options.withdrawal_day)
+    return datetime.date.today() + dateutil.relativedelta.relativedelta(
+        months=month_offset, day=options.withdrawal_day
+    )
+
 
 def do_check_balance():
     session = requests.session()
-    if (options.provider in ["dialog", "dialog-new"]):
+    if options.provider in ["dialog", "dialog-new"]:
         check_http_reply(session.get("https://stats.tis-dialog.ru/"))
         post_data = {"login": options.login, "passv": options.password}
-        check_http_reply(session.post("https://stats.tis-dialog.ru/index.php", data=post_data))
-        response = session.get("https://stats.tis-dialog.ru/index.php?phnumber={}".format(options.login))
+        check_http_reply(
+            session.post("https://stats.tis-dialog.ru/index.php", data=post_data)
+        )
+        response = session.get(
+            "https://stats.tis-dialog.ru/index.php?phnumber={}".format(options.login)
+        )
         check_http_reply(response)
         # Fix encoding
         response.encoding = response.apparent_encoding
         # Easy way to find xpath is to save reply as html, open in Chrome and
         # use "Inspect" > right click "Copy" > "Copy XPath"
-        balance_str = lxml.etree.HTML(response.text).xpath('/html/body/div/main/div[2]/table[1]/tr[4]/td[2]')[0].text
-        print_verbose(u"Balance as string: {}".format(balance_str))
-        balance = float(balance_str.replace(u" руб.", u"").replace(u",", u"."))
+        balance_str = (
+            lxml.etree.HTML(response.text)
+            .xpath("/html/body/div/main/div[2]/table[1]/tr[4]/td[2]")[0]
+            .text
+        )
+        print_verbose("Balance as string: {}".format(balance_str))
+        balance = float(balance_str.replace(" руб.", "").replace(",", "."))
         if options.provider == "dialog-new":
             response = session.get("https://stats.tis-dialog.ru/index.php?mod=payments")
             check_http_reply(response)
             response.encoding = response.apparent_encoding
             if options.verbose:
                 print_verbose("Deposits and withdrawals:")
-                for table_row in lxml.etree.HTML(response.text).xpath('/html/body/div/main/div[2]/table[1]/tr'):
-                    print(u"    " + u"; ".join(i.text for i in table_row.getchildren()))
-            # lxml.etree.tostring(lxml.etree.HTML(response.text).xpath('/html/body/div/main/div[2]/table[1]/tr[td[1][text() = "07.07.2023"]]')[2])
+                for table_row in lxml.etree.HTML(response.text).xpath(
+                    "/html/body/div/main/div[2]/table[1]/tr"
+                ):
+                    print("    " + "; ".join(i.text for i in table_row.getchildren()))
             # import ipdb
             # ipdb.set_trace()
             todays_withdrawal_entry = lxml.etree.HTML(response.text).xpath(
@@ -78,23 +98,48 @@ def do_check_balance():
                     # "07.07.2023",
                     # .today()?
                     datetime.datetime.now().strftime("%d.%m.%Y"),
-                    "-{},00".format("{:,d}".format(options.withdrawal_amount).replace(",", " "))
+                    "-{},00".format(
+                        "{:,d}".format(options.withdrawal_amount).replace(",", " ")
+                    ),
                 )
             )
             todays_withdrawal = len(todays_withdrawal_entry) != 0
+            last_withdrawal_amount = (
+                lxml.etree.HTML(response.text)
+                .xpath(
+                    '/html/body/div/main/div[2]/table[1]/tr[td[2][starts-with(text(),"-")]]/td[2]'
+                )[0]
+                .text
+            )
+            print_verbose(f"Last withdrawal amount: {last_withdrawal_amount}")
         session.get("https://stats.tis-dialog.ru/index.php?mod=exit")
     else:
         check_http_reply(session.get("https://stat.sovatelecom.ru/"))
         post_data = {
-            "LOGIN": options.login, "PASSWD": options.password,
-            "URL": "stat.sovatelecom.ru", "domain": "", "subm": "Вход"
+            "LOGIN": options.login,
+            "PASSWD": options.password,
+            "URL": "stat.sovatelecom.ru",
+            "domain": "",
+            "subm": "Вход",
         }
-        check_http_reply(session.post("https://stat.sovatelecom.ru/login_user.htms", data=post_data))
+        check_http_reply(
+            session.post("https://stat.sovatelecom.ru/login_user.htms", data=post_data)
+        )
         response = session.get("https://stat.sovatelecom.ru/main.htms")
         check_http_reply(response)
-        balance_str = lxml.etree.HTML(response.text).xpath('//*[@id="onyma_stat_main_fin"]/table[1]/tr[2]/td[1]/table[2]/tr[1]/td[1]/table[1]/tr[last()]/td[2]')[0].text
-        print_verbose(u"Balance as string: {}".format(balance_str))
-        balance = float(balance_str.replace(u"Остаток\xa0:\xa0", u"").replace(u" RUB", u"").replace(u",", u""))
+        balance_str = (
+            lxml.etree.HTML(response.text)
+            .xpath(
+                '//*[@id="onyma_stat_main_fin"]/table[1]/tr[2]/td[1]/table[2]/tr[1]/td[1]/table[1]/tr[last()]/td[2]'
+            )[0]
+            .text
+        )
+        print_verbose("Balance as string: {}".format(balance_str))
+        balance = float(
+            balance_str.replace("Остаток\xa0:\xa0", "")
+            .replace(" RUB", "")
+            .replace(",", "")
+        )
 
     # # DEBUG
     # balance = 5.57
@@ -118,8 +163,13 @@ def do_check_balance():
             next_withdrawal_date,
             days_until_withdrawal,
             options.login,
-            options.withdrawal_amount
+            options.withdrawal_amount,
         )
+        if last_withdrawal_amount != f"-{options.withdrawal_amount},00":
+            return (
+                STATUS_CRITICAL,
+                f"Unexpected withdrawal of {last_withdrawal_amount}. {status_text}",
+            )
         if balance < options.withdrawal_amount:
             if days_until_withdrawal < options.critical_threshold:
                 return (STATUS_CRITICAL, status_text)
@@ -131,30 +181,79 @@ def do_check_balance():
             return (STATUS_OK, status_text)
     else:
         if balance < options.critical_threshold:
-            return (STATUS_CRITICAL, "Balance {} is less than critical threshold of {}".format(balance, options.critical_threshold))
+            return (
+                STATUS_CRITICAL,
+                "Balance {} is less than critical threshold of {}".format(
+                    balance, options.critical_threshold
+                ),
+            )
         elif balance < options.warning_threshold:
-            return (STATUS_WARNING, "Balance {} is less than warning threshold of {}".format(balance, options.warning_threshold))
+            return (
+                STATUS_WARNING,
+                "Balance {} is less than warning threshold of {}".format(
+                    balance, options.warning_threshold
+                ),
+            )
         else:
             return (STATUS_OK, "Balance is {}".format(balance))
+
 
 def main():
     exit_code = STATUS_OK
     try:
-        parser = argparse.ArgumentParser(description="Custom script to check balance and write status to a file")
+        parser = argparse.ArgumentParser(
+            description="Custom script to check balance and write status to a file"
+        )
         parser.add_argument("login", help="User name")
         parser.add_argument("password", help="password")
-        parser.add_argument('-p', '--provider', dest='provider', default="dialog",
-            choices=["dialog", "dialog-new", "sovatel"], help='ISP type (default: %(default)s)')
-        parser.add_argument('-w', '--warning-threshold', dest='warning_threshold', type=int, default=200,
-            help='Minimal balance/days to cause warning state (default: 200₽ or 10 days)')
-        parser.add_argument('-c', '--critical-threshold', dest='critical_threshold', type=int, default=100,
-            help='Minimal balance/days to cause critical state (default: 100₽ or 5 days)')
-        parser.add_argument('-d', '--withdrawal-day', dest='withdrawal_day', type=int, default=1,
-            help='Day of monthly withdrawal (dialog-new only, default: %(default)d)')
-        parser.add_argument('-a', '--withdrawal-amount', dest='withdrawal_amount', type=int, default=455,
-            help='Amount of monthly withdrawal (dialog-new only, default: %(default)d)')
-        parser.add_argument('-v', '--verbose', dest='verbose', action='store_true', default=False,
-            help='Display verbose debug messages')
+        parser.add_argument(
+            "-p",
+            "--provider",
+            dest="provider",
+            default="dialog",
+            choices=["dialog", "dialog-new", "sovatel"],
+            help="ISP type (default: %(default)s)",
+        )
+        parser.add_argument(
+            "-w",
+            "--warning-threshold",
+            dest="warning_threshold",
+            type=int,
+            default=200,
+            help="Minimal balance/days to cause warning state (default: 200₽ or 10 days)",
+        )
+        parser.add_argument(
+            "-c",
+            "--critical-threshold",
+            dest="critical_threshold",
+            type=int,
+            default=100,
+            help="Minimal balance/days to cause critical state (default: 100₽ or 5 days)",
+        )
+        parser.add_argument(
+            "-d",
+            "--withdrawal-day",
+            dest="withdrawal_day",
+            type=int,
+            default=1,
+            help="Day of monthly withdrawal (dialog-new only, default: %(default)d)",
+        )
+        parser.add_argument(
+            "-a",
+            "--withdrawal-amount",
+            dest="withdrawal_amount",
+            type=int,
+            default=455,
+            help="Amount of monthly withdrawal (dialog-new only, default: %(default)d)",
+        )
+        parser.add_argument(
+            "-v",
+            "--verbose",
+            dest="verbose",
+            action="store_true",
+            default=False,
+            help="Display verbose debug messages",
+        )
 
         global options
         options = parser.parse_args()
@@ -168,9 +267,11 @@ def main():
             if options.critical_threshold == 100:
                 options.critical_threshold = 5
 
-        print_verbose("Checking balance for user {} on {}".format(options.login, options.provider))
+        print_verbose(
+            "Checking balance for user {} on {}".format(options.login, options.provider)
+        )
 
-        check_status,check_message = do_check_balance()
+        check_status, check_message = do_check_balance()
         exit_code = check_status
         print_verbose(f"Status: {STATUS_NAMES[check_status]}")
         print(check_message)
@@ -181,7 +282,8 @@ def main():
         print("Unhandled exception: {}".format(e))
         exit_code = STATUS_CRITICAL
 
-    return (exit_code)
+    return exit_code
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     sys.exit(main())
