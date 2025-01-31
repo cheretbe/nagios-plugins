6063059bb3ac5392360bfbdf9c881aef16c4b167
cheretbe <a.a.orlov@gmail.com>
2025-01-31 21:45:10 +0200

    check_seafile_sync: Fix status line split + black formatting

--- check_seafile_sync/check_seafile_sync.py
+++ check_seafile_sync/check_seafile_sync.py
@@ -9,10 +9,11 @@ import json
 import subprocess
 
 # Nagios status codes
-STATUS_UNKNOWN  = -1
-STATUS_OK       =  0
-STATUS_WARNING  =  1
-STATUS_CRITICAL =  2
+STATUS_UNKNOWN = -1
+STATUS_OK = 0
+STATUS_WARNING = 1
+STATUS_CRITICAL = 2
+
 
 def main(args):
     exit_status = STATUS_OK
@@ -31,24 +32,24 @@ def main(args):
 
         for line in subprocess.check_output(("seaf-cli", "status"), universal_newlines=True).splitlines():
             if line[0] != "#":
-                library,status = line.split()[0:2]
+                library, status = line.split(maxsplit=1)
                 if library not in seafile_status:
                     # Strip microseconds to emulate Linux 'date -Iseconds' behavior
                     seafile_status[library] = datetime.datetime.now().replace(microsecond=0)
                 # https://github.com/haiwen/seafile/blob/master/daemon/sync-mgr.c#L473
                 if exit_status_text:
-                    exit_status_text += '; '
+                    exit_status_text += "; "
 
                 # if library == "projects":
-                    # seafile_status[library] = datetime.datetime.now().replace(microsecond=0) - datetime.timedelta(minutes=100)
-                    # seafile_status[library] = datetime.datetime.now().replace(microsecond=0) - datetime.timedelta(days=1, minutes=10)
-                    # status = "committing"
+                # seafile_status[library] = datetime.datetime.now().replace(microsecond=0) - datetime.timedelta(minutes=100)
+                # seafile_status[library] = datetime.datetime.now().replace(microsecond=0) - datetime.timedelta(days=1, minutes=10)
+                # status = "committing"
 
                 if status == "synchronized":
                     exit_status_text += f"{library}: {status}"
                     seafile_status[library] = datetime.datetime.now().replace(microsecond=0)
                 elif status in ["committing", "initializing", "downloading", "merging", "uploading"]:
-                    time_delta = datetime.datetime.now().replace(microsecond=0) - seafile_status[library] 
+                    time_delta = datetime.datetime.now().replace(microsecond=0) - seafile_status[library]
                     status_icon = ""
                     if time_delta.total_seconds() > (args.warning * 60):
                         status_icon = "(!)"
@@ -75,10 +76,15 @@ def main(args):
     print(exit_status_text)
     sys.exit(exit_status)
 
+
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
-    parser.add_argument("-w", "--warning", type=int, default=30, help="Warning threshold in minutes (default=30)")
-    parser.add_argument("-c", "--critical", type=int, default=90, help="Warning threshold in minutes (default=90)")
+    parser.add_argument(
+        "-w", "--warning", type=int, default=30, help="Warning threshold in minutes (default=30)"
+    )
+    parser.add_argument(
+        "-c", "--critical", type=int, default=90, help="Warning threshold in minutes (default=90)"
+    )
     args = parser.parse_args()
 
     main(args)
